d73b837a5fb3dfc9277ac12a0f40190d15893af0
huchelmann <>
2007-02-12 10:49:30 +0000

    mise en place de la fifo

--- fifo.c
+++ fifo.c
@@ -1,8 +1,35 @@
 
 #include "fifo.h"
 
+
 uint8_t* cmd_fifo;
 uint32_t cmd_fifo_idx;
+
+
+
+//creer la fifo dasn les variable globales
+void creerFifo(){
+
+int numshm;
+
+ cmd_fifo_idx = 0;
+
+ numshm = shmget(IPC_PRIVATE,TAILLEMEM,0666);
+ cmd_fifo = shmat(numshm,NULL,(uint8_t)NULL);
+
+}
+
+//l'ecritu dans la fifo se fait a l'aide d ela macrot OUTPUT_FIFO
+
+void lectureFifo(void * buff,int idx){
+
+
+  memcpy(buff,&cmd_fifo[idx],sizeof(cmd_fifo[idx]));
+  idx=(cmd_fifo_idx+1)%TAILLEMEM;
+
+
+}
+
 
 
 
--- fifo.h
+++ fifo.h
@@ -2,15 +2,22 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
+#define TAILLEMEM 128*1024
+ 
 extern uint8_t* cmd_fifo;
 extern uint32_t cmd_fifo_idx;
+
+void creerFifo();
+
 
 #define OUTPUT_FIFO(A,S) \
 	do{\
 	/* here check there is enough room or wait or rewind or... */\
 	memcpy(&cmd_fifo[cmd_fifo_idx],A,S);\
-	cmd_fifo_idx+=S;\
+	cmd_fifo_idx=(cmd_fifo_idx+S)%TAILLEMEM;\
 	}while(0)
 
 
