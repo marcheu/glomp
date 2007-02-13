61550621049205d5fc144c4bbfb2a8dc1d1e59bd
huchelmann <>
2007-02-13 15:36:14 +0000

    *** empty log message ***

--- fifo.h
+++ fifo.h
@@ -4,11 +4,15 @@
 #include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/types.h>
 
 #define TAILLEMEM 128*1024
  
 extern uint8_t* cmd_fifo;
+extern uint8_t* buffer;
 extern uint32_t cmd_fifo_idx;
+extern uint32_t idx; //indice client
 
 void creerFifo();
 
@@ -18,6 +22,13 @@ void creerFifo();
 	/* here check there is enough room or wait or rewind or... */\
 	memcpy(&cmd_fifo[cmd_fifo_idx],A,S);\
 	cmd_fifo_idx=(cmd_fifo_idx+S)%TAILLEMEM;\
+	}while(0)
+
+#define INPUT_FIFO(S) \
+	do{\
+	/* here check there is enough room or wait or rewind or... */\
+	memcpy(&buffer,cmd_fifo[idx],S);\
+	idx=(idx+S)%TAILLEMEM;\
 	}while(0)
 
 
--- init.c
+++ init.c
@@ -1,11 +1,100 @@
+#include "fifo.h"
 
-// the number of rendering clients
+#define NBCARTE 4
+#define SEMKEY  64
+
+unit8_t* cmd_fifo;
+uint32_t cmd_fifo_idx;
 int client_num;
 
-void glomp_init()
-{
-	// open the config file
+
+//variable global pour le semaphore
+int            sem_id ;			
+struct sembuf  sem_oper_P;	/* Operation P */	
+struct sembuf  sem_oper_V;	/* Operation V */
+
 	
 
-}
 
+
+void glop_init(){
+  /*tout d abord il faut ouvir les fichier de config*/
+
+
+
+  //creation d'un pere et de 4 fils 
+  //le pere etant le producteur dans le fifo
+  //les fils serotn 4 consomateurs
+
+
+
+  /*1ere etape, creation du semaphore, enfin de ces structures*/
+  union semun 
+  {
+    int val;
+    struct semid_ds *buf;
+    ushort *array;
+  } s_ctl;
+  
+  /* Initialisations des structures ad-hoc pour faire P et V */ 
+  s_ctl.val          =  1;       /* Valeur d'initialisation du compteur */
+  sem_oper_P.sem_num =  0;       
+  sem_oper_P.sem_op  = -1;       /* Pour faire P */
+  sem_oper_V.sem_num =  0;
+  sem_oper_V.sem_op  =  1;       /* Pour faire V */
+
+  /* Creation et initialisation du tableau de  semaphores.
+   *   Ici, on utilise un seul semaphore, donc le second parametre est 
+   *   positionne a 1
+   */
+  
+  sem_id = semget (SEMKEY, 1, 0777|IPC_CREAT);
+  /**/
+
+
+
+
+
+
+
+
+  client_num=NBCARTE;
+  //avant le fork, on creer le shm
+  creerFifo();
+
+  /*boucle de creation des process*/
+
+    int i;
+    i=0;
+    int varfork;
+    varfork=1;
+
+    for(i;i<NBCARTE;i++){
+      if(varfork==0){
+	//on est dans un des processus fils
+
+
+      }
+      else{
+	//on est dasn le pere
+	varfork=fork();
+	if(varfork==-1){perror("fork");return -1;}
+      }
+
+    }
+    /*process creer*/
+
+
+
+   
+    if(varfork==0){
+      
+    }
+    else{
+
+    }
+
+  
+
+
+} 
